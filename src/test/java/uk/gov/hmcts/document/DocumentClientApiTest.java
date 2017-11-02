0c9470c868bf8eb675481a9b11343106031816da
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-11-02 11:38:30 +0000

    Change to Spring Annotation, need tests fixing

--- src/test/java/uk/gov/hmcts/document/DocumentClientApiTest.java
+++ src/test/java/uk/gov/hmcts/document/DocumentClientApiTest.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.document;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import feign.Contract;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.document.domain.Document;
 import uk.gov.hmcts.document.domain.UploadResponse;
@@ -27,33 +29,34 @@ public class DocumentClientApiTest {
     @Test
     public void clientConnects() throws IOException {
         stubFor(post(urlEqualTo("/documents"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBody(new ResourceReader().read("/fileuploadresponse.json"))));
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withBody(new ResourceReader().read("/fileuploadresponse.json"))));
 
 
-        final MultipartFile file = new InMemoryMultipartFile("000LR002.pdf", new byte[]{65, 66, 67, 68});
+        final MultipartFile file = new InMemoryMultipartFile("000LR002.pdf", new byte[] {65, 66, 67, 68});
 
         final MultipartFile file2
-                = new InMemoryMultipartFile("000LR050.pdf", new byte[]{69, 70, 71, 72, 73, 68});
+            = new InMemoryMultipartFile("000LR050.pdf", new byte[] {69, 70, 71, 72, 73, 68});
 
         final UploadResponse response = Feign.builder()
-                .encoder(new FeignSpringFormEncoder())
-                .decoder(new JacksonDecoder())
-                .target(DocumentClientApi.class, "http://localhost:8083")
-                .upload("token here", new MultipartFile[]{file, file2});
+            .contract(new SpringMvcContract())
+            .encoder(new FeignSpringFormEncoder())
+            .decoder(new JacksonDecoder())
+            .target(DocumentClientApi.class, "http://localhost:8083")
+            .upload("token here", new MultipartFile[] {file, file2});
 
         final Document firstDocument = response.embedded.documents.get(0);
         assertThat(firstDocument.originalDocumentName).isEqualTo("000LR002.pdf");
 
         assertThat(firstDocument.links.self.href)
-                .isEqualTo("http://localhost:8085/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4");
+            .isEqualTo("http://localhost:8085/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4");
 
         final Document secondDocument = response.embedded.documents.get(1);
         assertThat(secondDocument.originalDocumentName).isEqualTo("000LR050.pdf");
 
         assertThat(secondDocument.links.self.href)
-                .isEqualTo("http://localhost:8085/documents/5cc80e35-cdb8-4933-967e-54cef53d4a57");
+            .isEqualTo("http://localhost:8085/documents/5cc80e35-cdb8-4933-967e-54cef53d4a57");
     }
 
 }
