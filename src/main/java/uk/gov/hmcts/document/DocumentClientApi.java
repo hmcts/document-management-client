a1ad5e9b72a62fb67574da1fdab3304404df1db7
Sabah Irfan <Sabah.Irfan@hmcts.net>
2017-11-03 12:03:47 +0000

    Add download binary client

--- src/main/java/uk/gov/hmcts/document/DocumentClientApi.java
+++ src/main/java/uk/gov/hmcts/document/DocumentClientApi.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.document;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,4 +23,7 @@ public interface DocumentClientApi {
                           @RequestParam("files") MultipartFile[] files,
                           @RequestParam("classification") String classification);
 
+    @RequestMapping(method = RequestMethod.GET, value = "/documents/{documentId}/binary")
+    ResponseEntity<Resource> downloadBinary(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+                                            @PathVariable("documentId") String documentId);
 }
