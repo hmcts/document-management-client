f78f080a4ae564489503b28351f0436b5dbaf39d
dharmendra kumar <dharmendra.kumar@hmcts.net>
2017-11-13 13:03:31 +0000

    refactored imports

--- src/main/java/uk/gov/hmcts/document/DocumentDownloadClientApi.java
+++ src/main/java/uk/gov/hmcts/document/DocumentDownloadClientApi.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.document;
 
-    import org.springframework.cloud.netflix.feign.FeignClient;
-    import org.springframework.core.io.Resource;
-    import org.springframework.http.HttpHeaders;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.PathVariable;
-    import org.springframework.web.bind.annotation.RequestHeader;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "document-management-download-gateway-api", url = "${document_management.api_gateway.url}")
 public interface DocumentDownloadClientApi {
