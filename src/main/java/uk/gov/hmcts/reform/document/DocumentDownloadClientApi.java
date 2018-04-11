fe929c4112219dc49506424b41572f8a182ace83
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-11 09:54:11 +0100

    Update spring boot and spring cloud versions

--- src/main/java/uk/gov/hmcts/reform/document/DocumentDownloadClientApi.java
+++ src/main/java/uk/gov/hmcts/reform/document/DocumentDownloadClientApi.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.document;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
