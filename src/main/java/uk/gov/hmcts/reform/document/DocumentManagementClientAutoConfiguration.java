c19d97cf7895d5d02eb9b68903cab876609d4b2d
mdayican <mdayican@gmail.com>
2026-08-28 11:59:57 +0100

    fix integration in guthub action

--- src/main/java/uk/gov/hmcts/reform/document/DocumentManagementClientAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/document/DocumentManagementClientAutoConfiguration.java
@@ -1,17 +1,29 @@
 package uk.gov.hmcts.reform.document;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.document.healthcheck.DocumentManagementHealthIndicator;
 
 @AutoConfiguration
 @ConditionalOnProperty(prefix = "document_management", name = "url")
 @EnableFeignClients(basePackages = "uk.gov.hmcts.reform.document")
-@Import(DocumentUploadClientApi.class)
 public class DocumentManagementClientAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DocumentUploadClientApi documentUploadClientApi(
+        @Value("${document_management.url}") String dmUri,
+        RestTemplate restTemplate,
+        ObjectMapper objectMapper
+    ) {
+        return new DocumentUploadClientApi(dmUri, restTemplate, objectMapper);
+    }
 
     @Bean
     public DocumentManagementHealthIndicator documentManagement(
