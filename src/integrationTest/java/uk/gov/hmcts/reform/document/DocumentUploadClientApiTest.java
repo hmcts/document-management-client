89398bc41a31aeec1c65a309ea6d3f40f38685bc
mdayican <mdayican@gmail.com>
2026-08-28 12:52:22 +0100

    fix integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/document/DocumentUploadClientApiTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/document/DocumentUploadClientApiTest.java
@@ -28,7 +28,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 @AutoConfigureWireMock(port = 0)
 @EnableAutoConfiguration
 @SpringBootTest(
-    classes = HttpConfiguration.class,
+    classes = {DocumentManagementClientAutoConfiguration.class, HttpConfiguration.class},
+    properties = {"document_management.url=http://localhost:${wiremock.server.port}"},
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 class DocumentUploadClientApiTest {
